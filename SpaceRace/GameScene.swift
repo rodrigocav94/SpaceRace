9bf57f8fbe7a4a55836aeb86b9c04c548656bc48
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-06-09 19:45:01 -0300

    Adjusted starfield particles to the ipad size, added a few properties

--- SpaceRace/GameScene.swift
+++ SpaceRace/GameScene.swift
@@ -8,6 +8,16 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var starfield: SKEmitterNode!
+    var player: SKSpriteNode!
+    
+    var scoreLabel: SKLabelNode!
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     override func didMove(to view: SKView) {
         
     }
